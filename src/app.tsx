ef66ecf2a879554a19b4c872fd3c17d17f1031ff
Mario Reder <mreder1289@gmail.com>
2021-07-15 07:15:07 +0200

    asset selector

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 
-import { Button, Page, Spacer, Text } from '@geist-ui/react';
+import * as Icon from '@geist-ui/react-icons';
+import {
+  Button,
+  Page,
+  Progress,
+  Row,
+  Spacer,
+  Text,
+  Tooltip
+} from '@geist-ui/react';
 
 export const App: FC = () => {
   let gameFileInput: HTMLInputElement | null = null;
+  const [gameFiles, setGameFiles] = useState<File[]>([]);
 
   const handleSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files) return;
     const file = event.target.files[0];
     if (!file) return;
-    console.log('FILE', file);
+    setGameFiles([file, ...gameFiles]);
+  };
+
+  const deleteFile = (fileName: string) => () => {
+    const files = [...gameFiles];
+    files.splice(
+      files.findIndex(file => file.name === fileName),
+      1
+    );
+    setGameFiles(files);
   };
 
   return (
@@ -19,25 +38,52 @@ export const App: FC = () => {
           This is a simple tool to extract and bundle all assets that are
           required to play on Shroom Kingdom.
         </Text>
+        <Spacer y={1} />
+        <Text>
+          Please select all your game resource files from Super Mario Maker 2:
+        </Text>
+        <div style={{ maxWidth: '24rem' }}>
+          {gameFiles.map(gameFile => (
+            <Row
+              key={gameFile.name}
+              style={{ marginBottom: '0.6rem' }}
+              justify="space-between"
+            >
+              <span>{gameFile.name}</span>
+
+              <Tooltip text={'Remove this entry'} type="dark">
+                <Button
+                  auto
+                  size="mini"
+                  icon={<Icon.Delete />}
+                  onClick={deleteFile(gameFile.name)}
+                />
+              </Tooltip>
+            </Row>
+          ))}
+        </div>
         <Button
           type="success-light"
+          icon={<Icon.PlusCircle />}
           onClick={() => {
             if (gameFileInput) {
               gameFileInput.click();
             }
           }}
         >
-          Select
+          Add
         </Button>
         <input
           id="smm-game-file-input"
           type="file"
-          accept=".xci"
+          accept=".xci,.nsp"
+          multiple
           ref={ref => (gameFileInput = ref)}
           style={{ display: 'none' }}
           onChange={handleSelect}
         />
         <Spacer y={2} />
+        <Progress value={50} />
       </Page>
     </>
   );
