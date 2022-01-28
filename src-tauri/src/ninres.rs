65604e4b2b771c42061fd0cc9d78fec0cdb36bf8
Mario Reder <mreder1289@gmail.com>
2022-01-28 11:30:02 +0100

    changes to image bundling
    - crop out sprite sheets into single files
    - max image compression
    - more parallelism

--- src-tauri/src/ninres.rs
+++ src-tauri/src/ninres.rs
@@ -1,7 +1,11 @@
 use crate::Result;
 
-use image::{DynamicImage, ImageBuffer, ImageOutputFormat};
+use image::{
+    png::{CompressionType, FilterType, PngEncoder},
+    DynamicImage, GenericImageView, ImageBuffer,
+};
 use ninres::{Bfres, EmbeddedFile, NinRes, NinResFile, Sarc};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use std::{
     cmp,
     io::Cursor,
@@ -37,6 +41,10 @@ fn extract_bfres(
         match file {
             EmbeddedFile::BNTX(bntx) => {
                 for texture in bntx.get_textures().iter() {
+                    let texture_name = texture.get_name();
+                    if texture_name.starts_with("WU_") {
+                        continue;
+                    }
                     for (tex_count, mips) in texture.get_texture_data().iter().enumerate() {
                         if let Some(mip) = mips.iter().next() {
                             let width = cmp::max(1, texture.width);
@@ -57,13 +65,83 @@ fn extract_bfres(
                                 .replace(".Nin_NX_NVN", "")
                                 .replace(path::MAIN_SEPARATOR, "_")
                                 .replace("Model_", "");
+
+                            if texture_name.contains("_Field_")
+                                && !texture_name.contains("_Field_anime_")
+                            {
+                                (0..48u32)
+                                    .into_par_iter()
+                                    .map(|y| -> Result<()> {
+                                        for x in 0..16 {
+                                            let image = image.crop_imm(x * 16, y * 16, 16, 16);
+
+                                            let transparency_bytes: Vec<_> = image
+                                                .as_bytes()
+                                                .iter()
+                                                .skip(3)
+                                                .step_by(4)
+                                                .copied()
+                                                .collect();
+                                            let transparent_bytes =
+                                                vec![0; transparency_bytes.len()];
+                                            if transparency_bytes == transparent_bytes {
+                                                continue;
+                                            }
+
+                                            let mut file_name = file_name.clone();
+                                            file_name.push_str(&format!(
+                                                "{}_{}_{}.png",
+                                                texture.get_name(),
+                                                tex_count,
+                                                x + 16 * y
+                                            ));
+
+                                            let mut image_data = vec![];
+                                            let encoder = PngEncoder::new_with_quality(
+                                                &mut image_data,
+                                                CompressionType::Best,
+                                                FilterType::NoFilter,
+                                            );
+                                            encoder.encode(
+                                                image.as_bytes(),
+                                                image.width(),
+                                                image.height(),
+                                                image.color(),
+                                            )?;
+
+                                            let mut header = tar::Header::new_gnu();
+                                            header.set_size(image_data.len() as u64);
+                                            header.set_mode(0o644);
+                                            header.set_mtime(mtime);
+                                            header.set_cksum();
+                                            builder.write().unwrap().append_data(
+                                                &mut header,
+                                                file_name.clone(),
+                                                &image_data[..],
+                                            )?;
+                                        }
+                                        Ok(())
+                                    })
+                                    .collect::<Result<()>>()?;
+                            }
                             file_name.push_str(&format!(
                                 "{}_{}.png",
                                 texture.get_name(),
                                 tex_count
                             ));
+
                             let mut image_data = vec![];
-                            image.write_to(&mut image_data, ImageOutputFormat::Png)?;
+                            let encoder = PngEncoder::new_with_quality(
+                                &mut image_data,
+                                CompressionType::Best,
+                                FilterType::NoFilter,
+                            );
+                            encoder.encode(
+                                image.as_bytes(),
+                                image.width(),
+                                image.height(),
+                                image.color(),
+                            )?;
 
                             let mut header = tar::Header::new_gnu();
                             header.set_size(image_data.len() as u64);
@@ -92,7 +170,7 @@ fn extract_sarc(
     mtime: u64,
 ) -> Result<()> {
     sarc.get_sfat_nodes()
-        .iter()
+        .into_par_iter()
         .map(move |sfat| -> Result<_> {
             let mut path = out_path.clone();
             if let Some(sfat_path) = sfat.get_path() {
